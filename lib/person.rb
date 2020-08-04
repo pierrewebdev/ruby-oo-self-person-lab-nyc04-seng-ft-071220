611c62cdc32d33968042228f4eea6bcff2808319
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 19:19:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,16 @@
 # your code goes here
 class Person
   attr_reader :name
-  attr_accessor :bank_account, :happiness
+  attr_accessor :bank_account
+  
+  #I have to make a custom setter for happiness
+  def happiness=(happiness)
+    if happiness <= 10
+      @happiness = 10
+    else
+      @happiness = 10
+  end
+  
   def initialize(name)
     @name = name
     @bank_account = 25
