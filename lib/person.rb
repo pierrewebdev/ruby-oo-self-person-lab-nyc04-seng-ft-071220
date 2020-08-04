a6e3f562effc278b3abaa04add210dc72d6758e4
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 19:30:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -19,6 +19,22 @@ class Person
     @happiness
   end
   
+  #I have to make a custom setter for hygiene
+  def hygiene=(hygiene)
+    if hygiene <= 10 && hygiene > 0
+      @hygiene = hygiene
+    elsif happiness < 0
+      @happiness = 0
+    elsif happiness > 10
+      @happiness = 10
+    end
+  end
+  
+  #getter to match my custom setter for hygiene
+  def happiness
+    @happiness
+  end
+  
   def initialize(name)
     @name = name
     @bank_account = 25
