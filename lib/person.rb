411873f4d75a267baa291bdc2fca0d693f1e73d6
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 19:09:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 # your code goes here
 class Person
-  attr_reader :name, :happiness
-  attr_accessor :bank_account
+  attr_reader :name
+  attr_accessor :bank_account, :happiness
   def initialize(name)
     @name = name
     @bank_account = 25
